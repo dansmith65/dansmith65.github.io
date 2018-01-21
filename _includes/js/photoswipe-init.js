7f05d6c41449861d52663e8fccf75629a04dca4e
Daniel Smith <dansmith65@gmail.com>
2018-01-20 19:07:14 -0700

    finalize PhotoSwipe integration

--- _includes/js/photoswipe-init.js
+++ _includes/js/photoswipe-init.js
@@ -119,6 +119,8 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             // define gallery index (for URL)
             galleryUID: galleryElement.getAttribute('id'),
 
+            tapToClose: true,
+
             getThumbBoundsFn: function(index) {
                 // See Options -> getThumbBoundsFn section of documentation for more info
                 var thumbnail = items[index].el,
