9194dae6e7ea61da894a9eb1873a2e5703cb4f1e
Daniel Smith <dansmith65@gmail.com>
2018-01-29 18:47:21 -0700

    convert subtopic_anchor into function

--- assets/js/src/subtopic_anchor.js
+++ assets/js/src/subtopic_anchor.js
@@ -1,13 +1,17 @@
-var els = document.querySelectorAll('h2, h3, h4, h5, h6'),
-id,
-a;
-for(var i = 0, l = els.length; i < l; i++) {
-    id = els[i].getAttribute('id');
-    if(id) {
-        a = document.createElement('a');
-        a.className = 'subtopic_anchor';
-        a.href = '#' + id;
-        a.innerText = '#';
-        els[i].appendChild(a);
+function subtopicAnchor (selector) {
+    var els = document.querySelectorAll(selector),
+        id,
+        a;
+    for(var i = 0, l = els.length; i < l; i++) {
+        id = els[i].getAttribute('id');
+        if(id) {
+            a = document.createElement('a');
+            a.className = 'subtopic_anchor';
+            a.href = '#' + id;
+            a.innerText = '#';
+            els[i].appendChild(a);
+        }
     }
 }
+
+subtopicAnchor('h2, h3, h4, h5, h6');
